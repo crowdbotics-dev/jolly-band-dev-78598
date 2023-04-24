279e3e439aa0d42b2cc506411ad0abf24f0f7808
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 06:33:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-band-dev-78598",
+  url: "https://jolly-band-dev-78598.botics.co",
+  api: "https://jolly-band-dev-78598.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
